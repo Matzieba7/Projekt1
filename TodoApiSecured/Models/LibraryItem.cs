d68f08b8be3568ed95f13bb2dab6f3d1aea38435
Mateusz Zięba <matzieba7@gmail.com>
2022-01-11 23:37:17 +0100

    first commit

---
+++ TodoApiSecured/Models/LibraryItem.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryApi.Models
+{
+    public class LibraryItem
+    {
+        [Key]   //assume identity
+        [Column(TypeName = "int")]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(200)]
+        [Column(TypeName = "nvarchar(200)")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        [Column(TypeName = "nvarchar(200)")]
+        public string Author { get; set; }
+
+        [StringLength(150)]
+        [Column(TypeName = "nvarchar(150)")]
+        public string Edition { get; set; }
+
+        [StringLength(200)]
+        [Column(TypeName = "nvarchar(200)")]
+        public string CheckedOutByWhom { get; set; }
+
+        [Column(TypeName = "int")]
+        public int Condition { get; set; }
+
+        [StringLength(50)]
+        [Column(TypeName = "nvarchar(50)")]
+        public string Category { get; set; }
+
+        [StringLength(200)]
+        [Column(TypeName = "nvarchar(200)")]
+        public string Description { get; set; }
+    }
+}
